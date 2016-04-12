9f3f4206cc3632e8cc9ab96e63d63cf5156495bd
Roman.Pianov <Roman.Pianov@billing.ru>
2016-04-12 21:51:35 +0300

    Добавлено несколько методов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -4,5 +4,16 @@ public class MyFirstProgramm {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        double l = 8;
+        double a = 34;
+        double b = 11;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " +  b + " = " + area(a, b));
+    }
+    public static double area (double len){
+        return len * len;
+    }
+    public static double area(double a, double b ){
+        return a * b;
     }
 }
