1dcbb6bd4e0a965633036413e659d9c022f869e9
Roman.Pianov <Roman.Pianov@billing.ru>
2016-04-27 17:24:48 +0300

    Изменение в NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -8,20 +9,26 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Created by roman.pianov on 19.04.2016.
  */
 public class NavigationHelper extends HelperBase {
-  private WebDriver wd;
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGropPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))  && wd.findElement(By.tagName("h1")).getText().equals("Groups") && isElementPresent(By.name("new"))){
+      return;
+    }
+      click(By.linkText("groups"));
   }
 
   public void returnToHomePage() {
     click(By.linkText("home page"));
   }
+
   public void goToHomePage(){
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
