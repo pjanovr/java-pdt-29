fc7668b2af75c5272c5b6b70feefa0df868b14d6
Roman.Pianov <Roman.Pianov@billing.ru>
2016-04-29 11:00:02 +0300

    Проверка в тесты по группам

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -9,15 +10,18 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupModificationTests extends TestBase {
 
   @Test
-  public void testGroupModification(){
+  public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()){
-      app.getGroupHelper().crateGroup(new GroupData("test1",null, null));
+    int before = app.getGroupHelper().getGroupCount();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().crateGroup(new GroupData("test1", null, null));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupFrom(new GroupData("test1","test1", "test1"));
+    app.getGroupHelper().fillGroupFrom(new GroupData("test1", "test1", "test1"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
   }
 }
