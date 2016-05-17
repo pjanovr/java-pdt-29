a46a5e10c9e0aea49240c9f66ea89087cf092f09
Roman.Pianov <Roman.Pianov@billing.ru>
2016-05-17 10:34:48 +0300

    генератор данных в формате xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,10 +1,16 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
+@XStreamAlias("group")
 public class GroupData {
+  @XStreamOmitField
+  private  int id = Integer.MAX_VALUE;
   private  String name;
   private  String header;
   private  String footer;
-  private  int id = Integer.MAX_VALUE;
+
 
   public GroupData withId(int id) {
     this.id = id;
