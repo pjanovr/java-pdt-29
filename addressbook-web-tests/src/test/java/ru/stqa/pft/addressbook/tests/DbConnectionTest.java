16c24a911eb475e4330a7244d5e8d85c859aef29
Roman.Pianov <Roman.Pianov@billing.ru>
2016-05-24 13:55:10 +0300

    jdbc connector test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -15,9 +15,18 @@ public class DbConnectionTest {
     Connection conn = null;
 
     try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
       Statement st = conn.createStatement();
       ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+      Groups groups = new Groups();
+     while (rs.next()){
 
+        groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name")).withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(groups);
 
       // Do something with the Connection
 
